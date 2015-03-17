5a65805352fe3d512aee8d5220fd7acb6e88e965
Super Liu <s5s5cn@gmail.com>
2015-03-17 18:13:43 +0800

    learn

---
+++ Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString, myString2;
+            myInteger = 17;
+            myString = "\"myiteger\" is";
+            myString2 = @"E:\win\BegVCSharp\code\Chapter03Code\Chapter03Code";
+            Console.WriteLine("{0} {1}.", myString, myInteger);
+            Console.WriteLine(myString2);
+            Console.ReadKey();
+        }
+    }
+}

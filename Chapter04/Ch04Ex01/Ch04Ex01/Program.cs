4e4d51859b8c2b2751671fa613ceefe1ccc730d1
Super Liu <s5s5cn@gmail.com>
2015-03-19 18:22:41 +0800

    learn

---
+++ Chapter04/Ch04Ex01/Ch04Ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+            Console.WriteLine("Integer less than 10? {0}", isLessThan10);
+            Console.WriteLine("Integer between 0 and 5? {0}", isBetween0And5);
+            Console.WriteLine("Exactly one of the above is true? {0}",
+                isLessThan10 ^ isBetween0And5);
+            Console.ReadKey();
+            //start:
+            //    int myInteger = 5;
+            //    goto addVal;
+            //writerResult:
+            //    Console.WriteLine("myInteger = {0}", myInteger);
+            //    goto start;
+            //addVal:
+            //    myInteger += 10;
+            //    goto writerResult;
+
+        }
+    }
+}

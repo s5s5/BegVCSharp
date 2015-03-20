f73ccc9a4ecdc904be285635456f7a21ff7188cc
Super Liu <s5s5cn@gmail.com>
2015-03-20 15:16:29 +0800

    learn

--- Chapter04/Ch04Ex08/Ch04Ex08/Program.cs
+++ Chapter04/Ch04Ex08/Ch04Ex08/Program.cs
@@ -10,62 +10,64 @@ namespace Ch04Ex08
     {
         static void Main(string[] args)
         {
-            double inputWidth = 0, inputHeight;
-            double screenWidth = 78;
-            bool screenWidthTrue = true;
-            double realCoord = 0.03;
-            double imagCoord = 0.05;
+            double realMax = 1.77;
+            double realMin = -0.6;
+            double imagMax = -1.2;
+            double imagMin = 1.2;
+            double realStep, imagStep;
             double realTemp, imagTemp, realTemp2, arg;
-
-            while (screenWidthTrue)
-            {
-                Console.WriteLine("Enter width:");
-                inputWidth = Convert.ToDouble(Console.ReadLine());
-                if (inputWidth <= screenWidth && inputWidth > 0)
-                {
-                    screenWidthTrue = false;
-                }
-            }
-            Console.WriteLine("Enter height:");
-            inputHeight = Convert.ToDouble(Console.ReadLine());
-
             int iterations;
-            for (double i = 1.2; i >= -(imagCoord * inputHeight); i -= imagCoord)
+
+            while (true)
             {
-                for (double j = -0.6; j <= ((realCoord * inputWidth) - 0.6); j += realCoord)
+                realStep = (realMax - realMin) / 78;
+                imagStep = (imagMax - imagMin) / 22;
+                for (double i = imagMin; i >= imagMax; i += imagStep)
                 {
-                    iterations = 0;
-                    realTemp = j;
-                    imagTemp = i;
-                    arg = (j * j) + (i * i);
-                    while ((arg < 4) && (iterations < 40))
+                    for (double j = realMin; j <= realMax; j += realStep)
                     {
-                        realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
-                                    - j;
-                        imagTemp = (2 * realTemp * imagTemp) - i;
-                        realTemp = realTemp2;
-                        arg = (realTemp * realTemp) + (imagTemp * imagTemp);
-                        iterations += 1;
+                        iterations = 0;
+                        realTemp = j;
+                        imagTemp = i;
+                        arg = (j * j) + (i * i);
+                        while ((arg < 4) && (iterations < 40))
+                        {
+                            realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
+                                        - j;
+                            imagTemp = (2 * realTemp * imagTemp) - i;
+                            realTemp = realTemp2;
+                            arg = (realTemp * realTemp) + (imagTemp * imagTemp);
+                            iterations += 1;
+                        }
+                        switch (iterations % 4)
+                        {
+                            case 0:
+                                Console.Write(".");
+                                break;
+                            case 1:
+                                Console.Write("o");
+                                break;
+                            case 2:
+                                Console.Write("O");
+                                break;
+                            case 3:
+                                Console.Write("@");
+                                break;
+                        }
                     }
-                    switch (iterations % 4)
-                    {
-                        case 0:
-                            Console.Write(".");
-                            break;
-                        case 1:
-                            Console.Write("o");
-                            break;
-                        case 2:
-                            Console.Write("O");
-                            break;
-                        case 3:
-                            Console.Write("@");
-                            break;
-                    }
+                    Console.Write("\n");
                 }
-                Console.Write("\n");
+                Console.WriteLine("realMin:{0};realMax:{1};imagMin:{2};imagMax:{3};",
+                    realMin,realMax, imagMin, imagMax);
+                Console.WriteLine("realMin:");
+                realMin = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("realMax:");
+                realMax = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("imagMin:");
+                imagMin = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("imagMax:");
+                imagMax = Convert.ToDouble(Console.ReadLine());
             }
-            Console.ReadKey();
         }
     }
 }

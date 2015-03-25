67d580011230cba820f5ce50ffd9f4a95fe2a975
Super Liu <s5s5cn@gmail.com>
2015-03-25 15:19:08 +0800

    learn

---
+++ Chapter05/Ch05Ex04/Ch05Ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames =
+            {
+                "Robert Barwell",
+                "Mike Parry",
+                "Jeremy Beacock"
+            };
+            int i;
+            Console.WriteLine("Here are {0} of my friends:",
+                              friendNames.Length);
+            //for (i = 0; i < friendNames.Length; i++)
+            //{
+            //    Console.WriteLine(friendNames[i]);
+            //}
+            foreach (var friendName in friendNames)
+            {
+                Console.WriteLine(friendName);
+            }
+            Console.ReadKey();
+        }
+    }
+}

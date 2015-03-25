e19c7c127a100cdda643642fa8a1a06907b2e82c
Super Liu <s5s5cn@gmail.com>
2015-03-25 15:19:44 +0800

    learn

---
+++ Chapter05/Ch05Ex08/Ch05Ex08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05Ex08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Word: YES...");
+            string word = Console.ReadLine();
+            word = word.Replace("yes", "no");
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}

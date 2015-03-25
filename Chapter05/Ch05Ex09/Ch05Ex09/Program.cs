9074808cada1159cea2eea7f3c2cf8a7cf8a61b0
Super Liu <s5s5cn@gmail.com>
2015-03-25 15:19:52 +0800

    learn

---
+++ Chapter05/Ch05Ex09/Ch05Ex09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05Ex09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter more Word:");
+            string words = Console.ReadLine();
+            string[] s = words.Split(' ');
+            foreach (var s1 in s)
+            {
+                Console.Write("\"{0}\"", s1);
+            }
+            Console.ReadKey();
+        }
+    }
+}

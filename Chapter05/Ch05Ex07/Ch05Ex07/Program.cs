1337589dd5bec9d3aac779b0bc016be55252e747
Super Liu <s5s5cn@gmail.com>
2015-03-25 15:19:34 +0800

    learn

---
+++ Chapter05/Ch05Ex07/Ch05Ex07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word, reversionWord = "";
+            Console.WriteLine("Enter Word:");
+            word = Convert.ToString(Console.ReadLine());
+            for (int i = (word.Length - 1); i >= 0; i--)
+            {
+                reversionWord += word[i];
+            }
+            Console.Write("{0}", reversionWord);
+            Console.ReadKey();
+        }
+    }
+}

a8c68bb30ca38cb6369a112bbca0c07a59f4e9a2
Super Liu <s5s5cn@gmail.com>
2015-03-31 10:56:10 +0800

    learn

---
+++ Chapter06/Ch06Ex06/Ch06Ex06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Bed Param!");
+                return;
+            }
+            string str = args[0];
+            int i = Convert.ToInt32(args[1]);
+            Console.WriteLine("string:{0}, int:{1}", str, i);
+            Console.ReadKey();
+        }
+    }
+}

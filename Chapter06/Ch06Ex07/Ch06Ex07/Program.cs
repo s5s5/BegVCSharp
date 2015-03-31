c6145d9c671b97e50612e87b7e2ec12117fce845
Super Liu <s5s5cn@gmail.com>
2015-03-31 10:56:21 +0800

    learn

---
+++ Chapter06/Ch06Ex07/Ch06Ex07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06Ex07
+{
+    class Program
+    {
+        delegate string ReadLineDelegate();
+
+        static void Main(string[] args)
+        {
+            ReadLineDelegate readLine = new ReadLineDelegate(Console.ReadLine);
+            Console.WriteLine("input");
+            string userInput = readLine();
+            Console.WriteLine("out:{0}", userInput);
+            Console.ReadKey();
+        }
+    }
+}

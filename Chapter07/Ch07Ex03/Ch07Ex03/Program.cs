152be9f1ee5e3d0c8480d756357c633558c2b78d
Super Liu <s5s5cn@gmail.com>
2015-04-01 17:20:58 +0800

    learn

---
+++ Chapter07/Ch07Ex03/Ch07Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("line {0}", i);
+                if (i == 5000)
+                    Console.WriteLine(args[999]);
+            }
+        }
+    }
+}

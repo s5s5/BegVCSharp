1036006420a07afd96b384e28b9e0ad056cf3c6c
Super Liu <s5s5cn@gmail.com>
2015-04-07 15:21:36 +0800

    learn

---
+++ Chapter09/Traffic/Traffic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddPassenger(new Compact());
+            AddPassenger(new SUV());
+            AddPassenger(new PassengerTrain());
+            //AddPassenger(new Pickup());
+            Console.ReadKey();
+        }
+
+        static void AddPassenger(IPassengerCarrier Vehicle)
+        {
+            Console.WriteLine(Vehicle.ToString());
+        }
+    }
+}
---
+++ Chapter09/Vehicles/Vehicles/Vehicles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public interface IPassengerCarrier { }
+    public interface IHeavyLoadCarrier { }
+    public abstract class Vehicle { }
+    public class Car : Vehicle { }
+    public class Train : Vehicle { }
+    public class Compact : Car, IPassengerCarrier { }
+    public class SUV : Car, IPassengerCarrier { }
+    public class Pickup : Car, IHeavyLoadCarrier { }
+    public class PassengerTrain : Train, IPassengerCarrier { }
+    public class FreighTrain : Train, IHeavyLoadCarrier { }
+    public class T424DoubleBogey : Train { }
+}

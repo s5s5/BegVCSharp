05b5fd6609ca1c1df6e1164e523cae9fc1dfc420
Super Liu <s5s5cn@gmail.com>
2015-04-09 17:34:58 +0800

    learn

---
+++ Chapter10/Ch10Ex03/Ch10Ex03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10Ex03
+{
+    class MyClass
+    {
+        private string myString;
+
+        public string ContainedString
+        {
+            set { myString = value; }
+        }
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from derived class)";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.ContainedString = "s5s5";
+            Console.WriteLine(myClass.GetString());
+
+            MyDerivedClass myDerivedClass = new MyDerivedClass();
+            myDerivedClass.ContainedString = "seedling";
+            Console.WriteLine(myDerivedClass.GetString());
+            
+            Console.ReadKey();
+        }
+    }
+}

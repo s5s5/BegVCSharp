25ddbcf977f58be6acc0d3ba5ed4996838240c6c
Super Liu <s5s5cn@gmail.com>
2015-04-15 12:46:51 +0800

    learn

---
+++ Chapter11/Ch11Ex03/Ch11Ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes testPrimes = new Primes();
+            Console.WriteLine(testPrimes);
+            Primes primesFrom2To1000 = new Primes(2, 1000);
+            foreach (long i in primesFrom2To1000)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}

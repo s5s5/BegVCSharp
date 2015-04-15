15bc951a05aab052ddb1870ec2db49d4d19803d7
Super Liu <s5s5cn@gmail.com>
2015-04-15 12:46:33 +0800

    learn

---
+++ Chapter11/Ch11CardLib/Ch11CardClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch11CardLib;
+
+namespace Ch11CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code for deep-copying section.
+            Deck deck1 = new Deck();
+            Deck deck2 = (Deck)deck1.Clone();
+            Console.WriteLine("The first card in the original deck is: {0}", deck1.GetCard(0));
+            Console.WriteLine("The first card in the cloned deck is: {0}", deck2.GetCard(0));
+            deck1.Shuffle();
+            Console.WriteLine("Original deck shuffled");
+            Console.WriteLine("The first card in the original deck is: {0}", deck1.GetCard(0));
+            Console.WriteLine("The first card in the cloned deck is: {0}", deck2.GetCard(0));
+            Console.ReadKey();
+        }
+    }
+}

10a2823e560a429b81e1614c21b15ae37760b0d4
Super Liu <s5s5cn@gmail.com>
2015-04-16 16:55:18 +0800

    learn

--- Chapter11/Ch11CardLib/Ch11CardLib/Card.cs
+++ Chapter11/Ch11CardLib/Ch11CardLib/Card.cs
@@ -57,5 +57,119 @@ namespace Ch11CardLib
         {
             return !(card1 == card2);
         }
+
+        public override bool Equals(object card)
+        {
+            return this == (Card)card;
+        }
+
+        public override int GetHashCode()
+        {
+            return (13 * (int)suit) + (int)rank;
+        }
+
+        public static bool operator >(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)
+                {
+                    if (card1.rank == Rank.Ace)
+                    {
+                        if (card2.rank == Rank.Ace)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (card2.rank == Rank.Ace)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return (card1.rank > card2.rank);
+                        }
+                    }
+                }
+                else
+                {
+                    return (card1.rank > card2.rank);
+                }
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trump))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static bool operator <(Card card1, Card card2)
+        {
+            return !(card1 >= card2);
+        }
+
+        public static bool operator >=(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)
+                {
+                    if (card1.rank == Rank.Ace)
+                    {
+                        if (card2.rank == Rank.Ace)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            if (card2.rank == Rank.Ace)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                return (card1.rank >= card2.rank);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return (card1.rank >= card2.rank);
+                    }
+                }
+                else
+                {
+                    return (card1.rank >= card2.rank);
+                }
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trump))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static bool operator <=(Card card1, Card card2)
+        {
+            return !(card1 > card2);
+        }
     }
 }

06a9e08a289550f417487459bb940f82c339a3bd
Super Liu <s5s5cn@gmail.com>
2015-04-17 17:37:23 +0800

    learn

---
+++ Chapter11/Ch11Ex06/Ch11Ex06/Person.cs
@@ -0,0 +1,71 @@
+﻿namespace Ch11Ex06
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+            : this("xixi", 8)
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        //public static bool operator ==(Person person1, Person person2)
+        //{
+        //    return person1.Age == person2.Age;
+        //}
+
+        //public static bool operator !=(Person person1, Person person2)
+        //{
+        //    return !(person1 == person2);
+        //}
+
+        //public override bool Equals(object obj)
+        //{
+        //    return this == (Person)obj;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return (int)Age;
+        //}
+
+        public static bool operator >(Person person1, Person person2)
+        {
+            return person1.Age > person2.Age;
+        }
+
+        public static bool operator <(Person person1, Person person2)
+        {
+            return person1.Age < person2.Age;
+        }
+
+        public static bool operator >=(Person person1, Person person2)
+        {
+            return !(person1 < person2);
+        }
+
+        public static bool operator <=(Person person1, Person person2)
+        {
+            return !(person1 > person2);
+        }
+    }
+}
---
+++ Chapter11/Ch11Ex06/Ch11Ex06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People();
+            people.Add(new Person("super", 33));
+            people.Add(new Person("tom", 18));
+            people.Add(new Person("coco", 60));
+            people.Add(new Person("fufu", 60));
+            people.Add(new Person("jack", 33));
+
+            foreach (DictionaryEntry entry in people)
+            {
+                Console.WriteLine("{0} {1}",
+                    ((Person)entry.Value).Name, ((Person)entry.Value).Age);
+            }
+
+            Console.WriteLine("-------");
+
+            Person[] persons = people.GetOldest();
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine("{0} {1}",
+                    person.Name, person.Age);
+            }
+
+            foreach (int age in people.Ages)
+            {
+                Console.WriteLine("{0}", age);
+            }
+            Console.ReadKey();
+        }
+    }
+}

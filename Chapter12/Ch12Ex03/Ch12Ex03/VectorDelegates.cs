1b8a0264a6eaf049b3219efb2c9957f1d9d622c8
Super Liu <s5s5cn@gmail.com>
2015-04-28 20:12:49 +0800

    learn

---
+++ Chapter12/Ch12Ex03/Ch12Ex03/VectorDelegates.cs
@@ -0,0 +1,30 @@
+﻿namespace Ch12Ex03
+{
+    public static class VectorDelegates
+    {
+        public static int Compare(Vector x, Vector y)
+        {
+            if (x.R > y.R)
+            {
+                return 1;
+            }
+            else if (x.R < y.R)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        public static bool TopRightQuadrant(Vector target)
+        {
+            if (target.Theta >= 0.0 && target.Theta <= 90.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

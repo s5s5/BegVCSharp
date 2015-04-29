b7766d3024e346f9a25346319ca5db8cc66593ed
Super Liu <s5s5cn@gmail.com>
2015-04-29 19:58:41 +0800

    learn

---
+++ Chapter13/Ch13Ex02/Ch13Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection myConnection = new Connection();
+            Display myDisplay = new Display();
+            myConnection.MessageArrived +=
+                new MessageHandler(myDisplay.DisplayMessage);
+            myConnection.Connect();
+            System.Threading.Thread.Sleep(200);
+            Console.ReadKey();
+        }
+    }
+}

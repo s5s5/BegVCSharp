ba391e2c2947804cbf302ecbef01f69c2b60cf1f
Super Liu <s5s5cn@gmail.com>
2015-04-29 19:58:22 +0800

    learn

---
+++ Chapter13/Ch13CardLib/Ch13CardClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch13CardLib;
+
+namespace Ch13CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck1 = new Deck();
+            try
+            {
+                Card myCard = deck1.GetCard(60);
+            }
+            catch (CardOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.DeckContents[0]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter13/Ch13CardLib/Ch13CardLib/CardOutOfRangeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ch13CardLib
+{
+    public class CardOutOfRangeException : Exception
+    {
+        private Cards deckContents;
+
+        public Cards DeckContents
+        {
+            get
+            {
+                return deckContents;
+            }
+        }
+
+        public CardOutOfRangeException(Cards sourceDeckContents)
+            : base("There are only 52 cards in the deck.")
+        {
+            deckContents = sourceDeckContents;
+        }
+    }
+}

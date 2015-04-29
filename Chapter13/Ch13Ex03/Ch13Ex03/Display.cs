0dd6d7c9e71e7fe1e076088720fe2ac2b491d80d
Super Liu <s5s5cn@gmail.com>
2015-04-29 19:58:51 +0800

    learn

---
+++ Chapter13/Ch13Ex03/Ch13Ex03/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13Ex03
+{
+    public class Display
+    {
+        public void DisplayMessage(object source, MessageArrivedEventArgs e)
+        {
+            Console.WriteLine("Message arrived from: {0}",
+                              ((Connection)source).Name);
+            Console.WriteLine("Message Text: {0}", e.Message);
+        }
+    }
+}
